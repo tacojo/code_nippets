d3668cdbd79664decf45d5b7e0882bfb907bbd83
tacojo <tamara.cojocaru@gmail.com>
2023-03-05 11:02:57 +0000

    Create call_function_by_name.py

---
+++ call_function_by_name.py
@@ -0,0 +1,14 @@
+def head_function(function_name,params):
+    map_functions = { 
+        'func1': func1, 
+        'func1': func2 
+        }
+    map_functions[function_name](params)
+
+def func1(params): 
+	print(10+params['id'])
+        
+def func2(params): 
+    print(20+params['id'])
+
+head_function('func1',{'id':2})
