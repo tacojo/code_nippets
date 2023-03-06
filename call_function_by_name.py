db8a36cdde5c85cb71f888e15d8392c6ffe062ae
tacojo <tamara.cojocaru@gmail.com>
2023-03-06 10:38:40 +0000

    Update call_function_by_name.py

--- call_function_by_name.py
+++ call_function_by_name.py
@@ -1,3 +1,7 @@
+###########################################################
+################### Standalone func #######################
+###########################################################
+
 def head_function(function_name,params):
     map_functions = { 
         'addition': addition, 
@@ -27,3 +31,43 @@ def run_list_of_functions(list_functions, params):
         head_function(f,params)
 
 run_list_of_functions(list_functions_1, {'x': 3, 'y': 5})
+
+###########################################################
+################### Within a class ########################
+###########################################################
+class MyDynamicFunctions:
+    def __init__(self, list_functions, params):
+        self.list_functions = list_functions
+        self.params = params
+
+    def head_function(self, function_name, params):
+        map_functions = { 
+            'addition': addition, 
+            'subtraction': subtraction,
+            'multiply': multiply,
+            'divide': divide,
+            }
+        map_functions[function_name](params)
+
+    def addition(self): 
+        print(params['x'] + params['y'])
+            
+    def subtraction(self): 
+        print(params['x'] - params['y'])
+
+    def multiply(self): 
+        print(params['x'] * params['y'])
+            
+    def divide(self): 
+        print(params['x'] / params['y'])    
+
+    def run_list_of_functions(self):
+        for f in self.list_functions:
+            head_function(f, self.params)
+
+list_functions_1 = ['addition','subtraction']
+list_functions_2 = ['multiply','divide']
+params =  {'x': 2, 'y': 15}
+
+a = MyDynamicFunctions(list_functions_1, params)
+a.run_list_of_functions()
